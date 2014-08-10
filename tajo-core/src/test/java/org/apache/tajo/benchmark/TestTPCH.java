3f80beeac58dbbb4750c3ec1e2c1746777967b4f
HyoungJun Kim <babokim@babokim-MacBook-Pro.local>
2014-08-10 21:52:39 +0900

    PROFILE: remove unused testcase

--- tajo-core/src/test/java/org/apache/tajo/benchmark/TestTPCH.java
+++ tajo-core/src/test/java/org/apache/tajo/benchmark/TestTPCH.java
@@ -26,8 +26,6 @@ import org.junit.experimental.categories.Category;
 
 import java.sql.ResultSet;
 
-import static org.junit.Assert.assertEquals;
-
 @Category(IntegrationTest.class)
 public class TestTPCH extends QueryTestCaseBase {
 
@@ -54,32 +52,5 @@ public class TestTPCH extends QueryTestCaseBase {
     ResultSet res = executeQuery();
     assertResultSet(res);
     cleanupQuery(res);
-  }
-
-  @Test
-  public void textTPCH2() throws Exception {
-    String q1 = "create table q2_minimum_cost_supplier_tmp1 (s_acctbal double, s_name text, n_name text, p_partkey int8, ps_supplycost double, p_mfgr text, s_address text, s_phone text, s_comment text)";
-    String q2 = "create table q2_minimum_cost_supplier_tmp2 (p_partkey int8, ps_min_supplycost double)";
-    String q3 = "create table q2_minimum_cost_supplier (s_acctbal double, s_name text, n_name text, p_partkey int8, p_mfgr text, s_address text, s_phone text, s_comment text)";
-
-    executeString(q1).close();
-    executeString(q2).close();
-    executeString(q3).close();
-
-    ResultSet res = executeString(
-        "insert overwrite into q2_minimum_cost_supplier \n" +
-            "select \n" +
-            "  t1.s_acctbal, t1.s_name, t1.n_name, t1.p_partkey, t1.p_mfgr, t1.s_address, t1.s_phone, t1.s_comment \n" +
-            "from \n" +
-            "  q2_minimum_cost_supplier_tmp1 t1 join q2_minimum_cost_supplier_tmp2 t2 \n" +
-            "on \n" +
-            "  t1.p_partkey = t2.p_partkey and t1.ps_supplycost=t2.ps_min_supplycost \n" +
-            "order by s_acctbal desc, n_name, s_name, p_partkey \n" +
-            "limit 100"
-    );
-
-    assertEquals("", resultSetToString(res));
-
-    res.close();
   }
 }
